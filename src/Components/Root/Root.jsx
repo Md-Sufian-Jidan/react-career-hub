8a2ce88d20a90fd3a7e4f1f7f429b4f0e722d95b
Md-Sufian-Jidan <jidanjiyaj03@gmail.com>
2024-03-22 20:00:25 +0600

    react router set up and handle the error

--- src/Components/Root/Root.jsx
+++ src/Components/Root/Root.jsx
@@ -1,10 +1,11 @@
 import { Outlet } from "react-router-dom";
 import Footer from "../Footer/Footer";
+import Header from "../Header/Header";
 
 const Root = () => {
     return (
         <div>
-            hello
+            <Header />
             <Outlet />
             <Footer />
         </div>
--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
 import {
   createBrowserRouter,
@@ -8,14 +7,26 @@ import {
 } from "react-router-dom";
 import Root from './Components/Root/Root.jsx';
 import Home from './Components/Home/Home.jsx';
+import AppliedJobs from './Components/AppliedJobs/AppliedJobs.jsx';
+import Statistic from './Components/statistic/Statistic.jsx';
+import ErrorPage from './Components/ErrorPage/ErrorPage.jsx';
 const router = createBrowserRouter([
   {
     path: '/',
     element: <Root></Root>,
+    errorElement: <ErrorPage />,
     children : [
       {
         path : '/',
         element: <Home />
+      },
+      {
+        path: '/applied',
+        element: <AppliedJobs />
+      },
+      {
+        path: '/statistic',
+        element: <Statistic />
       }
     ]
   }
@@ -23,7 +34,6 @@ const router = createBrowserRouter([
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    {/* <App /> */}
     <RouterProvider router={router}></RouterProvider>
   </React.StrictMode>,
 )
